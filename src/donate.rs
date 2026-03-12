a982ad2dbe55f45bff52a1ed0a6fb1959c8a2cf4
glowcouch <42631203+glowcouch@users.noreply.github.com>
2026-03-13 08:46:28 +1300

    feat: make sponsor carousel

--- src/donate.rs
+++ src/donate.rs
@@ -11,19 +11,38 @@ pub(crate) fn Donate() -> View {
                     "Sponsor Us!"
                 }
             }
-            r#box::Box(class="mx-auto mt-8 lg:m-8 lg:absolute lg:right-0 lg:top-1/2 lg:-translate-y-1/2 w-fit h-fit") {
-                iframe(loading="lazy", src="https://givealittle.co.nz/widget/light-skyscraper/cause/oats-to-vex-worlds", class="w-70 h-130")
+            r#box::Box(class="mx-auto mt-8 lg:my-8 lg:ml-auto lg:mr-8 w-fit h-fit") {
+                iframe(loading="lazy", src="https://givealittle.co.nz/widget/light-skyscraper/cause/oats-to-vex-worlds", class="w-70 h-124")
                 span(class="sr-only") {"give a little"}
-                img(src="./assets/givealittle_logo.webp", class="w-40 mt-8 mx-auto", alt="", aria-hidden="true")
             }
-            r#box::Box(class="lg:absolute m-8 lg:bottom-0 lg:m-0 lg:left-8 lg:bottom-8 lg:w-130 text-white text-xl") {
-                p {"Getting a team of students from Tauranga to St. Louis is expensive. The cost is approximately $7000 NZD a student including a $1800 USD registration. Every dollar brings these students one step closer to the world stage."}
-                p {"Donations will go directly to helping students fundraise and pay for:"}
-                ul(class="list-disc pl-5") {
-                    li {"Return flights from New Zealand to St. Louis"}
-                    li {" Official tournament accommodation"}
-                    li {" Registration and competition entry fees"}
-                    li {" Robot equipment costs"}
+            div(class="lg:absolute m-8 lg:bottom-0 lg:m-0 lg:left-8 lg:bottom-8") {
+                r#box::Box(class="lg:w-130 text-white text-xl mb-8") {
+                    p {"Getting a team of students from Tauranga to St. Louis is expensive. The cost is approximately $7000 NZD a student including a $1800 USD registration. Every dollar brings these students one step closer to the world stage."}
+                    p {"Donations will go directly to helping students fundraise and pay for:"}
+                    ul(class="list-disc pl-5") {
+                        li {"Return flights from New Zealand to St. Louis"}
+                        li {" Official tournament accommodation"}
+                        li {" Registration and competition entry fees"}
+                        li {" Robot equipment costs"}
+                    }
+                }
+                div(class="relative flex") {
+                    div(class="text-white text-3xl whitespace-nowrap") {"Our sponsors:"}
+                    div(class="overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]") {
+                        div(class="flex w-max animate-sponsor-scroll hover:[animation-play-state:paused]") {
+                            div(class="w-[200vw] flex justify-between") {
+                                img(src="./assets/finn.webp", class="h-12 mx-10")
+                                img(src="./assets/indie.webp", class="h-12 mx-10")
+                                img(src="./assets/william.webp", class="h-12 mx-10")
+                                img(src="./assets/tom.webp", class="h-12 mx-10")
+
+                                img(src="./assets/finn.webp", class="h-12 mx-10")
+                                img(src="./assets/indie.webp", class="h-12 mx-10")
+                                img(src="./assets/william.webp", class="h-12 mx-10")
+                                img(src="./assets/tom.webp", class="h-12 mx-10")
+                            }
+                        }
+                    }
                 }
             }
         }
